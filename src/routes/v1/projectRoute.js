6b4893f9ff95f724163c625b6dc034caaee7a613
rakibul-dev <mdrakibul.dev@gmail.com>
2024-03-01 11:07:05 +0600

    refactor: add new routes

--- src/routes/v1/projectRoute.js
+++ src/routes/v1/projectRoute.js
@@ -1,11 +1,14 @@
 const {
   createProject,
   getProjects,
+  getProjectAnalytics,
+  deleteProject,
 } = require("../../controllers/projectController");
 const express = require("express");
 const router = express.Router();
 
 router.get("/", getProjects);
 router.post("/", createProject);
-
+router.delete("/:id", deleteProject);
+router.get("/analytics/:id", getProjectAnalytics);
 module.exports = router;
